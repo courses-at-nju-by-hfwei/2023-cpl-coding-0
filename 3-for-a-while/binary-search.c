50b47dc493a9e6921e06196a2e54334cc97f91fe
hengxin(Hengfeng Wei) <hfwei@nju.edu.cn>
2023-10-12 00:02:22 +0800

    +3-for-a-while

---
+++ 3-for-a-while/binary-search.c
@@ -0,0 +1,41 @@
+//
+// Created by hfwei on 2023/10/11.
+//
+
+#include <stdio.h>
+
+#define LEN 10
+int dict[LEN] = {1, 1, 2, 3, 5, 8, 13, 21, 35, 56};
+
+int main(void) {
+  int key = 0;
+  scanf("%d", &key);
+
+  // TODO: binary search
+  int low = 0;
+  int high = LEN - 1;
+
+  int index = -1;
+
+  while (low <= high) {
+    int mid = (low + high) / 2;
+
+    if (key > dict[mid]) {
+      low = mid + 1;
+    } else if (key < dict[mid]) {
+      high = mid - 1;
+    } else {  // key == dict[mid]
+      index = mid;
+      // break;  // what if `break` is removed
+      high = mid - 1; // find the leftmost index of the key
+    }
+  }
+
+  if (index == -1) {
+    printf("Not found!\n");
+  } else {
+    printf("The index of %d is %d.\n", key, index);
+  }
+
+  return 0;
+}
---
+++ 3-for-a-while/primes.c
@@ -0,0 +1,35 @@
+//
+// Created by hfwei on 2023/10/11.
+//
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+  int max = 0;
+  scanf("%d", &max);
+
+  // TODO: print primes between 1 and max
+
+  int count = 0;
+
+  for (int number = 2; number <= max; number++) {
+    // decide whether number is a prime
+    bool is_prime = true;
+    for (int factor = 2; factor * factor <= number; factor++) {
+      if (number % factor == 0) {
+        is_prime = false;
+        // break;  // test: number = 18
+      }
+    }
+
+    if (is_prime) { // TODO: is_prime == 1; is_prime != 0
+      count++;
+      printf("%d ", number);  // TODO: comment this for performance
+    }
+  }
+
+  printf("\ncount = %d\n", count);
+
+  return 0;
+}
